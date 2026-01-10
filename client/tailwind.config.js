1c140a1e48ed396be515d2f489e325142c14b710
Hosting en Linea <aledherrera@gmail.com>
2026-01-10 22:37:02 +0000

    Agregando rediseño 3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,11 +9,14 @@ export default {
       colors: {
         // Colores exactos de tu referencia MDS Global
         mds: {
-          dark: '#030816',    // Fondo Sidebar
+          dark: '#030816',    // Fondo Sidebar (Casi negro)
           blue: '#2563EB',    // Botón Azul Brillante
           hover: '#1e293b',   // Hover menú
           text: '#94a3b8'     // Texto grisáceo
         }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
       }
     },
   },
