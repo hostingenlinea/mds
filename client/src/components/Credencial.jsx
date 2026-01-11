6df72e2d9330cfc32fad3713a25957f966d111f7
Hosting en Linea <aledherrera@gmail.com>
2026-01-11 02:30:07 +0000

    Fix credenciales

--- client/src/components/Credencial.jsx
+++ client/src/components/Credencial.jsx
@@ -17,7 +17,6 @@ const Credencial = ({ pastor }) => {
             <div className="text-center relative z-10">
                 {/* Texto MDS (Solo MDS) */}
                 <h1 className="text-white font-serif font-bold text-3xl tracking-widest">MDS</h1>
-                <p className="text-blue-200 text-[10px] font-bold uppercase tracking-[0.3em] mt-1">Global</p>
             </div>
         </div>
 
